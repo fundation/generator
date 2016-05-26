f71cce157a6116bb5fbb9788ec9ff77df8953269
Alex Chan <arcannon@gmail.com>
2016-05-26 00:44:28 -0700

    Support for a port when declaring the app

--- templates/app.js
+++ templates/app.js
@@ -1,16 +1,19 @@
 'use strict';
 
-// Required libraries
-var fundation      = require('fundation');
-var app            = require('express')();
+module.exports = function(port) {
+  // Required libraries
+  var fundation = require('fundation');
+  var app = require('express')();
 
-// Add fundation to express
-app.use(fundation.init());
+  // Add fundation to express
+  app.use(fundation.init());
 
-// Start the server
-var port = process.env.PORT || 8080;
-app.listen(port, function(err) {
-  if ( err ) {
-    console.log(err);
-  }
-});
+  // Start the server
+  app.listen(port, function(err) {
+    if ( err ) {
+      console.log(err);
+    }
+  });
+
+  return app;
+}
